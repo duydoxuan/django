aee51bd1c29824e2ca0eb9b3047e587e2ea5744b
Ubuntu <ubuntu@ip-10-42-225-195.ec2.internal>
2019-12-29 16:37:22 +0000

    add custom user profile model, manager and migrations

--- script.sh
+++ script.sh
@@ -4,4 +4,6 @@
 #source ~/env/bin/activate
 #django-admin.py startproject profiles_project .
 #python3 manage.py startapp profiles_api
-
+#python manage.py runserver 0.0.0.0:8000
+#python manage.py makemigrations profiles_api
+#python manage.py migrate
